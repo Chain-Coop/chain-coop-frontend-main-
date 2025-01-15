5cea98fe6c01049bd392fd4c3ae84c53ad441926
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2025-01-15 20:21:24 +0100

    did some ui fixes

--- src/components/pages/entry/Landing.tsx
+++ src/components/pages/entry/Landing.tsx
@@ -416,7 +416,7 @@ const Landing = () => {
           >
             <motion.h1
               variants={fadeInUp}
-              className="mx-auto text-center text-lg font-bold tracking-wide sm:mt-2 md:text-lg lg:w-[50%] lg:text-[1.6em]"
+              className="text-md mx-auto text-center font-bold tracking-wide sm:mt-2 md:text-lg lg:w-[50%] lg:text-[1.6em]"
             >
               Join and Multiply Your Savings with our exclusive Dollar Group
               Cycles.
