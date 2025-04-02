7819c35bcabfaff37affd985abb819319c3d1259
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-04-02 13:16:03 +0100

    fixed dashboard layout

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -15,7 +15,7 @@ const Profile = () => {
   return (
     <main className="font-sans">
       <section>
-        <DashboardHeader className="flex h-[2.5em] w-full items-center justify-between bg-text2 px-[1.5em] font-sans text-xl font-semibold text-text5 lg:mt-[2em]">
+        <DashboardHeader className="flex items-center justify-between px-10 text-2xl  md:text-3xl lg:mt-[2em] lg:text-xl">
           <Link to="/dashboard/notification">
             <button
               className="relative inline-flex items-center"
