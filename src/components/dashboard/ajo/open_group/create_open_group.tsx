d21e7770d4f12b7771927f08152079c47c88b989
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-04-05 16:05:39 +0100

    pull

--- src/components/dashboard/ajo/open_group/create_open_group.tsx
+++ src/components/dashboard/ajo/open_group/create_open_group.tsx
@@ -3,26 +3,33 @@ import { Link } from "react-router-dom";
 import { FaAngleLeft } from "react-icons/fa6";
 import { Button } from "@material-tailwind/react";
 
-import createImage from "../../../../Assets/png/dashboard/ajo/open_group_image.png"
-import rightArrow from "../../../../Assets/svg/dashboard/ajo/right_arrow.svg"
-import FirstOpenGroupForm from "../components/first_open_group_form"
-import prevFormIcon from "../../../../Assets/svg/dashboard/ajo/prev_form.svg"
-import { useEffect, useState } from "react"
+import createImage from "../../../../Assets/png/dashboard/ajo/open_group_image.png";
+import rightArrow from "../../../../Assets/svg/dashboard/ajo/right_arrow.svg";
+import FirstOpenGroupForm from "../components/first_open_group_form";
+import prevFormIcon from "../../../../Assets/svg/dashboard/ajo/prev_form.svg";
+import { useEffect, useState } from "react";
 import SecondOpenGroupForm from "../components/second_open_group_form";
 import ThirdOpenGroupForm from "../components/third_open_group_form";
-import { firstOpenGroupType, secondOpenGroupType, thirdOpenGroupType } from "../../../../shared/types/types";
-import { validateFirstForm, validateSecondForm, validateThirdForm } from "../components/form_validation";
+import {
+  firstOpenGroupType,
+  secondOpenGroupType,
+  thirdOpenGroupType,
+} from "../../../../shared/types/types";
+import {
+  validateFirstForm,
+  validateSecondForm,
+  validateThirdForm,
+} from "../components/form_validation";
 import ReviewOpenGroupForm from "../components/review_open_group_form";
 import SuccessModal from "../components/success_modal";
 
-
 const CreateOpenGroup = () => {
-    const [firstFormData, setFirstFormData] = useState<firstOpenGroupType>({
-        savings_title: "",
-        savings_description: "",
-        savings_currency: "",
-        currency_image: ""
-    })  
+  const [firstFormData, setFirstFormData] = useState<firstOpenGroupType>({
+    savings_title: "",
+    savings_description: "",
+    savings_currency: "",
+    currency_image: "",
+  });
 
   const [secondFormData, setSecondFormData] = useState<secondOpenGroupType>({
     total_saving_amount: "",
@@ -37,47 +44,64 @@ const CreateOpenGroup = () => {
     agree: false,
   });
 
-    // state to toggle whether the next button is disabled or not
-    const [isNextDisabled, setIsNextDisabled] = useState<boolean>(true)
+  // state to toggle whether the next button is disabled or not
+  const [isNextDisabled, setIsNextDisabled] = useState<boolean>(true);
 
-    // state to open modal
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(true)
+  // state to open modal
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
-    // list of the various forms
-    const formSteps = [
-        <FirstOpenGroupForm data={firstFormData} setData={setFirstFormData} key={0} />,
-        <SecondOpenGroupForm data={secondFormData} setData={setSecondFormData} key={1} />,
-        <ThirdOpenGroupForm data={thirdFormData} currency={firstFormData.savings_currency} setData={setThirdFormData} key={2} />,
-        <ReviewOpenGroupForm firstForm={firstFormData} secondForm={secondFormData} thirdForm={thirdFormData} key={3} />
-    ]
+  // list of the various forms
+  const formSteps = [
+    <FirstOpenGroupForm
+      data={firstFormData}
+      setData={setFirstFormData}
+      key={0}
+    />,
+    <SecondOpenGroupForm
+      data={secondFormData}
+      setData={setSecondFormData}
+      key={1}
+    />,
+    <ThirdOpenGroupForm
+      data={thirdFormData}
+      currency={firstFormData.savings_currency}
+      setData={setThirdFormData}
+      key={2}
+    />,
+    <ReviewOpenGroupForm
+      firstForm={firstFormData}
+      secondForm={secondFormData}
+      thirdForm={thirdFormData}
+      key={3}
+    />,
+  ];
 
-    // this state controls which form is rendered
-    const [formStepsIndex, setFormStepsIndex] = useState<number>(2)
+  // this state controls which form is rendered
+  const [formStepsIndex, setFormStepsIndex] = useState<number>(2);
 
-    const validateInputs = () => {
-        // perform different validation checks based on the current form step
-        if (formStepsIndex === 0) {
-            setIsNextDisabled(validateFirstForm(firstFormData))
-        } else if (formStepsIndex === 1) {
-            setIsNextDisabled(validateSecondForm(secondFormData))
-        } else if (formStepsIndex === 2) {
-            setIsNextDisabled(validateThirdForm(thirdFormData))
-        }
+  const validateInputs = () => {
+    // perform different validation checks based on the current form step
+    if (formStepsIndex === 0) {
+      setIsNextDisabled(validateFirstForm(firstFormData));
+    } else if (formStepsIndex === 1) {
+      setIsNextDisabled(validateSecondForm(secondFormData));
+    } else if (formStepsIndex === 2) {
+      setIsNextDisabled(validateThirdForm(thirdFormData));
     }
+  };
 
-    useEffect(() => {
-        validateInputs()
-    }, [firstFormData, secondFormData, thirdFormData]);
+  useEffect(() => {
+    validateInputs();
+  }, [firstFormData, secondFormData, thirdFormData]);
 
-    const nextForm = () => {
-        setFormStepsIndex((prev) => prev + 1)
-        validateInputs() // validate inputs
-    }
+  const nextForm = () => {
+    setFormStepsIndex((prev) => prev + 1);
+    validateInputs(); // validate inputs
+  };
 
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
-
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <main className="mb-[20px] flex flex-col gap-10 font-sans">
@@ -112,35 +136,39 @@ const CreateOpenGroup = () => {
       {/* ACTIVE FORM */}
       {formSteps[formStepsIndex]}
 
-            <div className={ `w-[100%] flex ${formStepsIndex > 0 ? 'justify-between' : 'justify-end'} items-center mt-[20px]`}>
-                {
-                    formStepsIndex > 0 && (
-                        <Button onClick={() => {setFormStepsIndex( formStepsIndex - 1 )}} className="bg-transparent shadow-none">
-                            <img src={prevFormIcon} alt="Previous form" className="w-[40px]" />
-                        </Button>
-                    )
-                }
-                {
-                    formStepsIndex > 2 ? (
-                        <Button 
-                            className={`bg-[#440080] rounded-lg text-white text-[18px] font-[500] tracking-tighter w-fit capitalize h-[47px] `}
-                            onClick={openModal}>
-                        Create group
-                    </Button>
-                    ) : (
-                        <Button className={`bg-[#440080] rounded-lg text-white text-[20px] font-[500] tracking-tighter w-[121px] h-[47px] capitalize ${isNextDisabled ? 'opacity-70 cursor-not-allowed' : ''}`}  onClick={nextForm} disabled={isNextDisabled}>
-                            Next
-                        </Button>
-                    )
-                }
-            </div>
-            {
-                isModalOpen && (
-                    <SuccessModal />
-                )
-            }
-        </main>
-    )
-}
+      <div
+        className={`flex w-[100%] ${formStepsIndex > 0 ? "justify-between" : "justify-end"} mt-[20px] items-center`}
+      >
+        {formStepsIndex > 0 && (
+          <Button
+            onClick={() => {
+              setFormStepsIndex(formStepsIndex - 1);
+            }}
+            className="bg-transparent shadow-none"
+          >
+            <img src={prevFormIcon} alt="Previous form" className="w-[40px]" />
+          </Button>
+        )}
+        {formStepsIndex > 2 ? (
+          <Button
+            className={`h-[47px] w-fit rounded-lg bg-[#440080] text-[18px] font-[500] capitalize tracking-tighter text-white `}
+            onClick={openModal}
+          >
+            Create group
+          </Button>
+        ) : (
+          <Button
+            className={`h-[47px] w-[121px] rounded-lg bg-[#440080] text-[20px] font-[500] capitalize tracking-tighter text-white ${isNextDisabled ? "cursor-not-allowed opacity-70" : ""}`}
+            onClick={nextForm}
+            disabled={isNextDisabled}
+          >
+            Next
+          </Button>
+        )}
+      </div>
+      {isModalOpen && <SuccessModal />}
+    </main>
+  );
+};
 
 export default CreateOpenGroup;
